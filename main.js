4d4e8b11e81053edfdaafb5c1faffcb8eb3c9c20
gorinelli <106194389+gorinelli@users.noreply.github.com>
2024-06-06 08:04:55 -0300

    06-06

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
-const botoes = document.querySelectorAll(".botao")
-console.log(botoes)
-for(let i = 0; i < botoes.length ; i++){
-  botoes[i].onclik = function(){
-    for (let j = 0; j < botoes.length ;j++){
-    botoes[j].classList.remove("ativo");
-    }
-    botoes[i].classList.add("ativo")
+const botoes = document.querySelectorAll(".botao");
+const conteudo = document.querySelectorAll(".aba-conteudo");
+for(let i = 0; 1 < botoes.length ; i++){
+    botoes[i].onclick = function() {
+        for (j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+            conteudo[j].classList.remove("ativo");
+        }
+        botoes[i].classList.add("ativo")
+        conteudo[i].classList.add("ativo")
     }
 }
-
-
-
-
-
-
