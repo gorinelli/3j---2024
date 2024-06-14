58cd735424d5ff184c90533ba09f9c6f273973f1
gorinelli <106194389+gorinelli@users.noreply.github.com>
2024-06-14 09:42:09 -0300

    14-02

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".aba-conteudo");
 const tempo = document.querySelectorAll(".contador");
+tempo[0].textContent = "OLÁ"
+const tempoObjetivo1= new Date(2024,8,11,22);
+const tempoObjetivo2= new Date(2024,9,12,21);
+const tempoObjetivo3= new Date(2024,10,13,20);
+const tempoObjetivo4= new Date(2024,11,14,19);
+const agora= new Date ();
+tempo[1].textContent = agora;
 console.log(tempo);
 for(let i = 0; 1 < botoes.length ; i++){ 
     botoes[i].onclick = function() {
